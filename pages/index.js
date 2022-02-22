e72b3b38124c1665d6640c292c21091b36ae3bab
sjooski <99781879+sjooski@users.noreply.github.com>
2022-02-22 18:08:20 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Beat Fleet" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to the Beat Fleet distro site.
         </p>
       </main>
 
